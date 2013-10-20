b245a94432ced40be8ac1434e4e5ca6c991f8475
Clemens Akens <clebert@me.com>
2013-10-21 01:51:00 +0200

    Update constants test

--- test/constants.test.js
+++ test/constants.test.js
@@ -8,79 +8,43 @@ var assert = require('assert'),
 suite('constants', function () {
 
     suite('.BUTTON', function () {
-        var BUTTON = constants.BUTTON;
+        var BUTTON = constants.BUTTON,
+            offsetByKey;
 
-        suite('.UP', function () {
+        offsetByKey = {
+            UP: 0,
+            ENTER: 1,
+            DOWN: 2,
+            RIGHT: 3,
+            LEFT: 4,
+            ESCAPE: 5
+        };
 
-            test('should be 0', function () {
-                assert.strictEqual(BUTTON.UP, 0);
-            });
-        });
-
-        suite('.ENTER', function () {
-
-            test('should be 1', function () {
-                assert.strictEqual(BUTTON.ENTER, 1);
-            });
-        });
-
-        suite('.DOWN', function () {
-
-            test('should be 2', function () {
-                assert.strictEqual(BUTTON.DOWN, 2);
-            });
-        });
-
-        suite('.RIGHT', function () {
-
-            test('should be 3', function () {
-                assert.strictEqual(BUTTON.RIGHT, 3);
-            });
-        });
-
-        suite('.LEFT', function () {
-
-            test('should be 4', function () {
-                assert.strictEqual(BUTTON.LEFT, 4);
-            });
-        });
-
-        suite('.ESCAPE', function () {
-
-            test('should be 5', function () {
-                assert.strictEqual(BUTTON.ESCAPE, 5);
+        Object.keys(offsetByKey).forEach(function (key) {
+            suite('.' + key, function () {
+                test('should be ' + offsetByKey[key], function () {
+                    assert.strictEqual(BUTTON[key], offsetByKey[key]);
+                });
             });
         });
     });
 
     suite('.MOTOR_PORT', function () {
-        var MOTOR_PORT = constants.MOTOR_PORT;
+        var MOTOR_PORT = constants.MOTOR_PORT,
+            offsetByKey;
 
-        suite('.A', function () {
+        offsetByKey = {
+            A: 1,
+            B: 2,
+            C: 4,
+            D: 8
+        };
 
-            test('should be 1', function () {
-                assert.strictEqual(MOTOR_PORT.A, 1);
-            });
-        });
-
-        suite('.B', function () {
-
-            test('should be 2', function () {
-                assert.strictEqual(MOTOR_PORT.B, 2);
-            });
-        });
-
-        suite('.C', function () {
-
-            test('should be 4', function () {
-                assert.strictEqual(MOTOR_PORT.C, 4);
-            });
-        });
-
-        suite('.D', function () {
-
-            test('should be 8', function () {
-                assert.strictEqual(MOTOR_PORT.D, 8);
+        Object.keys(offsetByKey).forEach(function (key) {
+            suite('.' + key, function () {
+                test('should be ' + offsetByKey[key], function () {
+                    assert.strictEqual(MOTOR_PORT[key], offsetByKey[key]);
+                });
             });
         });
     });
