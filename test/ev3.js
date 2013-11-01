c8b008fa613700e8729ef3030cc218577eb0739b
Clemens Akens <clebert@me.com>
2013-11-01 14:12:46 +0100

    Update ev3.js

--- test/ev3.js
+++ test/ev3.js
@@ -2,12 +2,32 @@
 
 var ev3 = require('../index');
 
-ev3.button.enter.on('press', function () {
-    console.log('Up button pressed!');
+console.log('ev3 started...');
 
-    console.log('Battery voltage: ' + ev3.battery.getVoltage());
+ev3.button.up.on('press', function () {
+    console.log('up button pressed!');
 });
 
-ev3.button.enter.on('release', function () {
-    console.log('Up button released!');
+ev3.button.enter.on('press', function () {
+    console.log('enter button pressed!');
+
+    console.log('battery voltage: ' + ev3.battery.getVoltage());
+});
+
+ev3.button.down.on('press', function () {
+    console.log('down button pressed!');
+});
+
+ev3.button.right.on('press', function () {
+    console.log('right button pressed!');
+});
+
+ev3.button.left.on('press', function () {
+    console.log('left button pressed!');
+});
+
+ev3.button.escape.on('press', function () {
+    console.log('escape button pressed!');
+
+    process.exit();
 });
