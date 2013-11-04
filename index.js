20a2cebc23787ae0772d48e58894a298ceef9046
Clemens Akens <clebert@me.com>
2013-11-04 17:17:02 +0100

    Update public api

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 'use strict';
 
-Object.defineProperties(exports, {
-    battery: {
-        value: require('./lib/battery')
-    },
-    button: {
-        value: require('./lib/button')
-    },
-    Motor: {
-        value: require('./lib/motor').Motor
-    }
-});
+exports.battery = require('./lib/battery');
+exports.button = require('./lib/button');
+exports.Motor = require('./lib/motor').Motor;
